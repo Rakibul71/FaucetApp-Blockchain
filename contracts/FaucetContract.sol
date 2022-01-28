c9d6a8964f82b2cfbdd9eb926bafb84d67ed17e7
Rakibul71 <rakibulislam202019@gmail.com>
2022-01-29 00:53:20 +0600

    funders commit

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,12 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // uint256 public funds = 1000;
+    address[] public funders;
+
     receive() external payable {}
 
-    function addFunds() external payable {}
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
 
-    function justTest() external pure returns (uint256) {
-        return 2 + 2;
+    function getAllFunders() external view returns (address[] memory) {
+        return funders;
     }
 }
