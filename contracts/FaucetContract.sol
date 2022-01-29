469650b26d9897130c1aee6145a5666418747dd2
Rakibul71 <rakibulislam202019@gmail.com>
2022-01-29 23:51:12 +0600

    prevent duplicate value

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,15 +2,39 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    address[] public funders;
+    uint256 public numberOfFounders;
+    mapping(address => bool) private funders;
+    mapping(uint256 => address) private lutfunders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        funders.push(msg.sender);
+        // uint256 index = numberOfFounders++;
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            uint256 index = numberOfFounders++;
+            funders[funder] = true;
+            lutfunders[index] = funder;
+        }
     }
 
     function getAllFunders() external view returns (address[] memory) {
-        return funders;
+        address[] memory _funders = new address[](numberOfFounders);
+
+        for (uint256 i = 0; i < numberOfFounders; i++) {
+            _funders[i] = lutfunders[i];
+        }
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        return lutfunders[index];
     }
 }
+
+// const instance = await Faucet.deployed()
+// instance.addFunds({from:accounts[0],value:"2000000000000000"})
+// instance.addFunds({from:accounts[1],value:"2000000000000000"})
+//instance.getFunderAtIndex(0)
+//instance.getAllFunders()
