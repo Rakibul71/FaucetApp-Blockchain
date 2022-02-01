fb59ddbf6c1b6e9d1a19db072960ba9f514275a0
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-02 02:25:54 +0600

    metamask and account commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+// import Web3 from "web3";
+import Web3 from "web3";
+
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null;
+
+      if (window.ethereum) {
+        provider = window.ethereum;
+
+        try {
+          await provider.request({ method: "eth_requestAccounts" });
+        } catch {
+          console.error("User denied account access!");
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider,
+      });
+    };
+    loadProvider();
+  }, []);
+
+  // console.log(web3Api.web3);
+
+  // get account
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          <h1>
+            <h1>{account ? account : "not connected"}</h1>
+          </h1>
+          <div className="balance-view is-size-2">
+            Current Balance : <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn ">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
